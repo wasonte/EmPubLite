718978fdf8bc5af80812c5011f2d83f8a9d7b35a
wasonte <wasonte@gmail.com>
2016-03-29 17:08:44 +0200

    Tutorial #9

---
+++ app/src/main/java/com/commonsware/empublite/SimpleContentFragment.java
@@ -0,0 +1,54 @@
+package com.commonsware.empublite;
+
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebViewFragment;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SimpleContentFragment extends WebViewFragment {
+
+    private static final String KEY_FILE = "file";
+
+    private static SimpleContentFragment newInstance(String file){
+        SimpleContentFragment f = new SimpleContentFragment();
+        Bundle args = new Bundle();
+
+        args.putString(KEY_FILE, file);
+        f.setArguments(args);
+
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View result = super.onCreateView(inflater, container, savedInstanceState);
+
+        getWebView().getSettings().setJavaScriptEnabled(true);
+        getWebView().getSettings().setSupportZoom(true);
+        getWebView().getSettings().setBuiltInZoomControls(true);
+        getWebView().loadUrl(getPage());
+
+        return result;
+    }
+
+    private String getPage(){
+        return (getArguments().getString(KEY_FILE));
+    }
+
+}
